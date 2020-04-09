9f1bc5816a38c8945d20383ca13255c9ab03a633
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-09 14:43:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "select projects.title, (sum(pledges.amount) - projects.funding_goal) from projects
 join pledges on projects.id = pledges.project_id
-group by projects.id 
+group by projects.id
 having (sum(pledges.amount) - projects.funding_goal)>= 0"
 
 
